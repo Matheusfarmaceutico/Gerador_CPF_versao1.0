7151433dbbeac9c413d369082abc03649cba6aa3
Matheus Alves <matheusalvesrs20@gmail.com>
2022-01-22 17:20:43 -0300

    Gerar_de_cpf_codigofonte
    Um gerador de CPF's válidos. Este é o código fonte escrito em python.

---
+++ Código _Fonte_Python/Gerador_CPF_Python_Código_01.py
@@ -0,0 +1,33 @@
+from random import randint
+while True:
+    try:
+        quantos = int(input('Quantos CPFs gostaria de gerar? '))
+    except:
+        print('Caracter Inválido! Tente novamente.')
+        continue
+    lista = []
+    for c in range(quantos):
+        cpf = str(randint(100000000,999999999))
+        novo_cpf = cpf
+        reverso = 10
+        total = 0
+        for index in range(19):
+            if index > 8:
+                index -= 9
+            total += int(novo_cpf[index]) * reverso
+            reverso -= 1
+            if reverso < 2:
+                reverso = 11
+                d = 11 - (total % 11)
+                if d > 9:
+                    d = 0
+                novo_cpf += str(d)
+                total = 0
+        lista.append(novo_cpf)
+    print(lista)
+    option = ' '
+    while option not in 'SsNn':
+        option = str(input("Quer continuar gerando Cpf's? [S/N]: "))
+    if option in 'Nn':
+        break
+    
